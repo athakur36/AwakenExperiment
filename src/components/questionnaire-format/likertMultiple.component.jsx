dec075e5b7557b50829060a9c7aed86d09f73da8
Gaurav Arora <gauravarora20@gmail.com>
2020-12-06 23:02:31 +0000

    switch conditions to render IV_survey based on the new data structure

---
+++ src/components/questionnaire-format/likertMultiple.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import QNA from '../question-type/qna.component';
+
+const useStyles = makeStyles({
+  root: {},
+  intro: {
+    marginBottom: '50px',
+  },
+  submit: {
+    marginTop: '50px',
+    marginBottom: '150px',
+  },
+});
+
+const LikertMultiple = ({ questData }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <p className={classes.intro}>{questData.intro}</p>
+      <h3>Likert Multiple</h3>
+      <form>
+        {questData.questions.map((question, index) => (
+          <QNA key={index} label={question} />
+        ))}
+      </form>
+      <Button className={classes.submit} color='primary' variant='contained'>
+        Submit and Proceed
+      </Button>
+    </div>
+  );
+};
+
+export default LikertMultiple;
--- src/pages/surveyPage.component.jsx
+++ src/pages/surveyPage.component.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
 import Sct from '../components/questionnaire-format/sct.component';
+import { IV_Surveys } from '../model/IV-Surveys-Data';
+import LikertMultiple from '../components/questionnaire-format/likertMultiple.component';
+import LikertMatrix from '../components/questionnaire-format/likertMatrix.component';
 
 const useStyles = makeStyles({
   surveyRoot: {
@@ -12,44 +15,23 @@ const useStyles = makeStyles({
   },
 });
 
-const questData = {
-  intro: `This study examines the relationship between digital conduct and the
-  mind, in terms of meaning-making, outlooks on life, self-awareness, and
-  consciousness traits, etc. Therefore, it is important that you are in the right mindset. Please
-  plan for the appropriate space and time. It takes about 45 min. Please
-  take the entire inventory when you are rested and not rushed, with no
-  interruptions, in one sitting. Please also make sure that the responses
-  communicate your authentic inner experience, not some superficial
-  clichés. Your responses will be treated with the highest confidentially.  The exercise has 3 parts: open-ended sentence completions,
-  multiple-choice questions, and social media interactions. We start with an invitation for self-reflection, which asks you to
-  complete 16 incomplete sentences thoughtfully and honestly with your own
-  words. There are no right or wrong responses. No need to write an essay
-  for each: completing the sentence with some 4-12 words is enough.`,
-  questions: [
-    'Raising a family',
-    'When I am criticized',
-    'Being with other people',
-    'The thing I like about myself is',
-    'What gets me into trouble is',
-    'Crime and delinquency could be halted if',
-    'Business and society',
-    'Rules are',
-    'I just can’t stand people who',
-    'My main problem is',
-    'People who step out of line',
-    'A partner has the right to...',
-    'If I were in charge',
-    'Technology',
-    'My conscience bothers me if',
-  ],
-};
-
 const SurveyPage = ({ match }) => {
   const classes = useStyles();
 
   return (
     <div className={classes.surveyRoot}>
-      <Sct questData={questData} />
+      {IV_Surveys.map((ivSurvey, index) => {
+        switch (ivSurvey.surveyType) {
+          case 'SCT':
+            return <Sct questData={ivSurvey.surveyData} />;
+          case 'LIKERT_MULTIPLE':
+            return <LikertMultiple questData={ivSurvey.surveyData} />;
+          case 'LIKERT_MATRIX':
+            return <LikertMatrix questData={ivSurvey.surveyData} />;
+          default:
+            return <div>Survey Type is Invalid</div>;
+        }
+      })}
     </div>
   );
 };
