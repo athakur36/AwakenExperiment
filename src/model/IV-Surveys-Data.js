15ea557fba5db6bab5f4ecb0e83d769473be4632
athakur36 <athakur36@gmail.com>
2020-12-11 23:56:52 -0800

    updating survey page

--- src/model/IV-Surveys-Data.js
+++ src/model/IV-Surveys-Data.js
@@ -1,7 +1,7 @@
 export const IV_Surveys = [
-	{
-		surveyData: {
-            intro: `This study examines the relationship between digital conduct and the
+  {
+    surveyData: {
+      intro: `This study examines the relationship between digital conduct and the
             mind, in terms of meaning-making, outlooks on life, self-awareness, and
             consciousness traits, etc. Therefore, it is important that you are in the right mindset. Please
             plan for the appropriate space and time. It takes about 45 min. Please
@@ -13,127 +13,67 @@ export const IV_Surveys = [
             complete 16 incomplete sentences thoughtfully and honestly with your own
             words. There are no right or wrong responses. No need to write an essay
             for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'Raising a family',
-              'When I am criticized',
-              'Being with other people',
-              'The thing I like about myself is',
-              'What gets me into trouble is',
-              'Crime and delinquency could be halted if',
-              'Business and society',
-              'Rules are',
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'SCT',
-        step: 1
+      questions: [
+        "Raising a family",
+        "When I am criticized",
+        "Being with other people",
+        "The thing I like about myself is",
+        "What gets me into trouble is",
+        "Crime and delinquency could be halted if",
+        "Business and society",
+        "Rules are",
+        "I just can’t stand people who",
+        "My main problem is",
+        "People who step out of line",
+        "A partner has the right to...",
+        "If I were in charge",
+        "Technology",
+        "My conscience bothers me if",
+      ],
     },
-    {
-		surveyData: {
-            intro: `asoidhashneed to write an essay
+    surveyType: "SCT",
+    step: 1,
+  },
+  {
+    surveyData: {
+      intro: `asoidhashneed to write an essay
             for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'LIKERT_MULTIPLE',
-        step: 2
+      questions: [
+        "I just can’t stand people who",
+        "My main problem is",
+        "People who step out of line",
+        "A partner has the right to...",
+      ],
     },
-    {
-		surveyData: {
-            intro: `asoidhashneed to write an essay
+    surveyType: "LIKERT_MULTIPLE",
+    step: 2,
+  },
+  {
+    surveyData: {
+      intro: `asoidhashneed to write an essay
             for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'LIKERT_MATRIX',
-        step: 3
+      questions: [
+        "I just can’t stand people who",
+        "My main problem is",
+        "Technology",
+        "My conscience bothers me if",
+      ],
     },
-    {
-		surveyData: {
-            intro: `asoidhashneed to write an essay
+    surveyType: "LIKERT_MATRIX",
+    step: 3,
+  },
+  {
+    surveyData: {
+      intro: `asoidhashneed to write an essay
             for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'LIKERT_MATRIX',
-        step: 4
+      questions: [
+        "I just can’t stand people who",
+        "My main problem is",
+        "Technology",
+        "My conscience bothers me if",
+      ],
     },
-    {
-		surveyData: {
-            intro: `asoidhashneed to write an essay
-            for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'LIKERT_MATRIX',
-        step: 5
-    },
-    {
-		surveyData: {
-            intro: `asoidhashneed to write an essay
-            for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'LIKERT_MATRIX',
-        step: 6
-    },
-    {
-		surveyData: {
-            intro: `asoidhashneed to write an essay
-            for each: completing the sentence with some 4-12 words is enough.`,
-            questions: [
-              'I just can’t stand people who',
-              'My main problem is',
-              'People who step out of line',
-              'A partner has the right to...',
-              'If I were in charge',
-              'Technology',
-              'My conscience bothers me if',
-            ],
-          },
-        surveyType: 'LIKERT_MATRIX',
-        step: 7
-	}
+    surveyType: "SCT",
+    step: 4,
+  },
 ];
